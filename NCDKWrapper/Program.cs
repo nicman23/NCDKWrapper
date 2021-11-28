43070a6017e53e0dc1b265e3caeb73e5c264ce97
Your Name <you@example.com>
2021-11-28 11:08:50 +0000

    remove windows bs newlines ffs and tabs add error logging

--- NCDKWrapper/Program.cs
+++ NCDKWrapper/Program.cs
@@ -3,81 +3,85 @@ using NCDK.QSAR;
 using System;
 using System.Linq;
 using System.Reflection;
+using System.IO;
 
 namespace NCDKWrapper
 {
-    class Program
+  class Program
+  {
+    /*
+     args: 0) Category (i.e. Molecule) currently only molecules are supported
+           1) Descriptor names (comma delimited)
+           2) SDF file names (space delimited)
+    */
+    static int Main(string[] args)
     {
-        /*
-         args: 0) Category (i.e. Molecule) currently only molecules are supported
-               1) Descriptor names (comma delimited)
-               2) SDF file names (space delimited)
-        */
-        static int Main(string[] args)
+      try
+      {
+        if (args.Length < 3)
         {
-            try
-            {
-                if (args.Length < 3)
-                {
-                    PrintHelp();
-                    return 1;
-                }
-                var descriptorsList = args[1].Split(',');
-                NCDK.QSAR.IMolecularDescriptor[] descriptorInstance = new NCDK.QSAR.IMolecularDescriptor[descriptorsList.Length];
-                // var descriptorInstance = new List<object>();
-                for (int i = 0; i < descriptorsList.Length; i++)
-                {
-                    var str = $"NCDK.QSAR.Descriptors.{args[0]}.{descriptorsList[i]}";
-                    var descriptorType = Type.GetType($"NCDK.QSAR.Descriptors.{args[0]}.{descriptorsList[i]}, NCDK");
-                    var paramCount = descriptorType.GetConstructors()[0].GetParameters().Length;
-                    descriptorInstance[i] = (IMolecularDescriptor)descriptorType.GetConstructors()[0].Invoke(new object[paramCount]);
-                }
-                for (int i = 2; i < args.Length; i++)
-                {
-                    using (var suppl = Chem.SDMolSupplier(args[i]))
-                    {
-                        foreach (var mol in suppl)
-                        {
-                            if (mol == null)
-                            {
-                                continue;
-                            }
-                            for (int z = 0; z < descriptorsList.Length; z++)
-                            {
-                                var res = descriptorInstance[z].Calculate(mol);
-                                foreach (var item in res)
-                                {
-																		if (z == 0) {
-	                                    Console.Write($"\"{item.Value.ToString()}\"");
-																		}
-                                    Console.Write($",\"{item.Value.ToString()}\"");
-//                                  Console.Write($",\"{item.Key.ToString()}\"");
-//                                  Console.Write($",\"{item.Key}:{item.Value.ToString()}\"");
-                                }
-                            }
-                            Console.Write("\n");
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Exception has occured: \n" + ex);
-						return (int)2;
-            }
-						return (int)0;
+          PrintHelp();
+          return 1;
         }
-        private static void PrintHelp()
+        var descriptorsList = args[1].Split(',');
+        NCDK.QSAR.IMolecularDescriptor[] descriptorInstance = new NCDK.QSAR.IMolecularDescriptor[descriptorsList.Length];
+        // var descriptorInstance = new List<object>();
+        for (int i = 0; i < descriptorsList.Length; i++)
         {
-            Assembly ass = Assembly.Load("NCDK");
-            foreach (var type in ass.GetTypes().Where(t => string.Equals(t.Namespace, "NCDK.QSAR.Descriptors.Moleculars", StringComparison.InvariantCultureIgnoreCase) && !t.IsNested))
-            {
-                if (type.Name == "ChiIndexUtils")
-                {
-                    continue;
-                }
-                Console.WriteLine(type.Name);
-            }
+          var str = $"NCDK.QSAR.Descriptors.{args[0]}.{descriptorsList[i]}";
+          var descriptorType = Type.GetType($"NCDK.QSAR.Descriptors.{args[0]}.{descriptorsList[i]}, NCDK");
+          var paramCount = descriptorType.GetConstructors()[0].GetParameters().Length;
+          descriptorInstance[i] = (IMolecularDescriptor)descriptorType.GetConstructors()[0].Invoke(new object[paramCount]);
         }
+        for (int i = 2; i < args.Length; i++)
+        {
+          using (var suppl = Chem.SDMolSupplier(args[i]))
+          {
+            foreach (var mol in suppl)
+            {
+              if (mol == null)
+              {
+                continue;
+              }
+              Console.Write($"\"{mol.Title.ToString()}\"");
+              for (int z = 0; z < descriptorsList.Length; z++)
+              {
+                try {
+                  var res = descriptorInstance[z].Calculate(mol);
+                  foreach (var item in res)
+                  {
+                    Console.Write($",\"{item.Value.ToString()}\"");
+//                  Console.Write($",\"{item.Key}:{item.Value.ToString()}\"");
+                  }
+                } catch (Exception ex) {
+                  using (TextWriter errorWriter = Console.Error) {
+                    Console.Write(",\"\"");
+                    errorWriter.WriteLine($"{ex}");
+                  }
+                }
+              }
+              Console.Write("\n");
+            }
+          }
+        }
+      } catch (Exception ex)
+      {
+        Console.WriteLine("Exception has occured: \n" + ex);
+        return (int)2;
+      }
+      return (int)0;
     }
+    private static void PrintHelp()
+    {
+      Assembly ass = Assembly.Load("NCDK");
+      foreach (var type in ass.GetTypes().Where(t => string.Equals(t.Namespace, "NCDK.QSAR.Descriptors.Moleculars", StringComparison.InvariantCultureIgnoreCase) && !t.IsNested))
+      {
+        if (type.Name == "ChiIndexUtils")
+        {
+          continue;
+        }
+        Console.WriteLine(type.Name);
+      }
+    }
+  }
 }
